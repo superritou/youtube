f6228fd587e5442303c5161829d907807674910d
superritou <102620856+superritou@users.noreply.github.com>
2022-04-14 19:18:15 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,7 +1,7 @@
 //console.clear();
 var refer = document.referrer.pathname;
     if (refer != "oboabo.forumforever.com") {
-        document.location = document.referrer;
+        document.location = "//google.fr";
     }
 
 //scorll element
